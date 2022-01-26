318240cbe0d69c3daefaf64ca4615b449648af30
Leo GRAVIER <legravier@IUTCLINFA2319.iut.local>
2022-01-26 11:10:23 +0100

    juhzef

--- src/main/java/tp5_cloud/webRessource.java
+++ src/main/java/tp5_cloud/webRessource.java
@@ -20,7 +20,6 @@ public class webRessource {
 
     /**
      * Method handling HTTP GET requests. The returned object will be sent
-     * to the client as "text/plain" media type.
      *
      * @return String that will be returned as a text/plain response.
      */
